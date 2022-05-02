d29eb9f6bd5e7c385847413beaeac49732907999
Carlos Alves <carlos.alves.one@gmail.com>
2022-05-02 08:24:44 +0100

    Updated classes student and student main

--- src/StudentMain.java
+++ src/StudentMain.java
@@ -6,37 +6,8 @@
  * Created..: 02/05/2022
  */
 
-/* declare class toy */
+/* declare class main app student */
 public class StudentMain {
 
-    /* declare instances variables */
-    private final Object average;
-    private final double programming;
-    private final double webDev;
-    private final double maths;
-    private final double algorithms;
-    private static boolean failedModule;
-    private final String name;
-    private final String department;
-    private final int age;
-    private final String userName;
-    private final int studentNumber;
-    private final boolean fullTime;
 
-    /* declare the construction function for this class */
-    public StudentMain(Object average, double programming, double webDev, double maths,
-                       double algorithms, String name, String department, int age, String userName,
-                       int studentNumber, boolean fullTime) {
-        this.average = average;
-        this.programming = programming;
-        this.webDev = webDev;
-        this.maths = maths;
-        this.algorithms = algorithms;
-        this.name = name;
-        this.department = department;
-        this.age = age;
-        this.userName = userName;
-        this.studentNumber = studentNumber;
-        this.fullTime = fullTime;
-    }
 }
