a98a515c0d9f937586e1b4d078efc97b59acb484
Carlos Alves <carlos.alves.one@gmail.com>
2022-05-06 12:11:09 +0100

    Create class Report

---
+++ src/Report.java
@@ -0,0 +1,13 @@
+
+/*
+ * Goldsmiths University of London
+ * Author...: Carlos Manuel de Oliveira Alves
+ * Student..: cdeol003
+ * Created..: 06/05/2022
+ */
+
+/* declare class report */
+public class Report {
+
+
+}
