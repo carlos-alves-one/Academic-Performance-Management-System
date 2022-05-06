10ce5c7ff24040ce405e5d2b522842435a344c5a
Carlos Alves <carlos.alves.one@gmail.com>
2022-05-06 12:19:06 +0100

    Create method getReport

--- src/Report.java
+++ src/Report.java
@@ -6,8 +6,14 @@
  * Created..: 06/05/2022
  */
 
+import java.util.ArrayList;
+
 /* declare class report */
 public class Report {
 
+    // method to create a report according the option selected from the menu
+    public static String getReport(ArrayList<Student> students, int choice) {
 
+        return "";
+    }
 }
